43c5c4a67e2f9db58901c5ab9ed1ae87c16415ab
Everett Carney <everett@mentaltangent.net>
2015-01-04 20:50:22 -0800

    Updating main display page

---
+++ freelance/mt_old/js/ghost.js
@@ -0,0 +1,208 @@
+$(document).ready(function() {
+	
+	var	prevX = 0, newX = 0;
+	var centerX = $(window).width() / 2;
+	var centerY = $(window).height() / 2;
+	// console.log("Center x: " + centerX);
+	// console.log("Center y: " + centerY);
+	$('#ghost').css('margin-left', centerX - 70);
+
+	var ghostSize;
+	// if (centerX <= 490 || centerY <= 300) {
+	// 	$('#ghost').css('margin-top', (centerY * .8));
+	// 	ghostSize = "S.6, .6, 0, 0";
+	// }
+	// else {
+	// 	$('#ghost').css('margin-top', (centerY * .9));
+	// 	ghostSize = "S1, 1, 0, 0";
+	// }
+
+	if (centerX <= 640) {
+		if (centerY <= 320) {
+			$('#ghost').css('margin-top', (centerY * .8));
+			ghostSize = "S.6, .6, 0, 0";
+			$('#ghost').css('margin-left', centerX - 45);
+		} else {
+			$('#ghost').css('margin-top', (centerY * .75));
+			ghostSize = "S1, 1, 0, 0";
+		}
+	} else {
+		$('#ghost').css('margin-top', (centerY * .85));
+		ghostSize = "S1, 1, 0, 0";
+	}
+
+	var ghostPaper = Raphael('ghost', 140, 110); // (divId, width, height)
+	var ghost = ghostPaper.set();
+	var eyes = ghostPaper.set();
+
+	// The following code draws the images for the ghost
+	var lEye = ghostPaper.ellipse(50, 50, 15, 13) // (x, y, rX, rY)
+			 .attr({
+			 	fill: '#fff',
+			 	'stroke-width': 0,
+			 	transform: ghostSize
+			 });
+	var rEye = ghostPaper.ellipse(110, 50, 15, 13)
+		 .attr({
+		 	fill: '#fff',
+		 	'stroke-width': 0,
+		 	transform: ghostSize
+		 });
+	var mouth = ghostPaper.path("M50,90 C50,95 110,95 110,90") // "M<origin point> C<control for origin point> <control for close point> <close point>"
+		.attr({
+			stroke: '#fff',
+			'stroke-width': 4,
+			transform: ghostSize
+		});
+
+	ghost.push(
+		lEye, rEye, mouth	
+	);
+
+	eyes.push(
+		lEye, rEye
+	);
+
+	// Directional looking set of animations
+	function lookRight() {
+		eyes.animate({'transform': 'T10,0'+ghostSize}, 300, center); 				// Animates the eyes
+		mouth.animate({'path': 'M55,90 C50,95 110,95 110,90'}, 300); 	// Animates the mouth
+	}
+
+	function lookLeft() {
+		eyes.animate({'transform': 'T-10, 0'+ghostSize}, 300, center); 			// Animates the eyes
+		mouth.animate({'path': 'M50,90 C50,95 110,95 105,90'}, 300); 	// Animates the mouth
+	}
+
+	function center() {
+		eyes.animate({'transform': 'T0,0'+ghostSize}, 300);
+		mouth.animate({'path': 'M50,90 C50,95 110,95 110,90'}, 300);
+	}
+
+	// This is where we call the animations based on the actions of the user
+	$("#main").mousewheel(function(event) {
+
+    	newX = this.scrollLeft;
+
+		if (newX > prevX)
+			lookRight();
+		else if (newX < prevX)
+			lookLeft();
+
+		prevX = newX;
+
+	});
+
+});
+
+// var p = new Raphael(0, 0, 400, 400);
+// p.rect(0, 0, 400, 400);
+
+// var redRect = p.rect(20, 20, 50, 50)
+// 	.attr({
+// 		fill: "red",
+// 		stroke: "none",
+// 		cursor: "pointer"
+// 	}).click(function() {
+// 		this.attr({fill: "blue"});
+// 	}).hover(function() {
+// 		this.attr({fill: "green"});
+// 	}, function() {
+// 		this.attr({fill: "red"});
+// 	});
+
+// p.circle(100, 100, 30)
+// 	.attr({
+// 		"stroke-width": 10,
+// 		fill: "45-#f00-#000"
+// 	}).animate({
+// 		cx: 300,
+// 		transform: "s2"
+// 	}, 1000, "elastic")
+// 	.click(function () {
+// 		redRect.animate({y: 200}, 1000);
+// 	});
+
+// p.rect(200, 300, 50, 50)
+// 	.attr({
+// 		fill: "orange"
+// 	}).animate({
+// 		transform: "r180",
+// 		fill: "blue"
+// 	}, 1000);
+
+// p.text(50, 250, "Hello there")
+// 	.attr({
+// 		"font-family": "Georgia",
+// 		"font-size": 20
+// 	}).animate({
+// 		y: 350
+// 	}, 500, "elastic");
+
+// var amount = 100;
+// var loc = 100;
+// var radius = 30;
+
+
+// var archtype = Raphael("canvas", 200, 200);
+// archtype.customAttributes.arc = function (xloc, yloc, value, total, R) {
+//     var alpha = 360 / total * value,
+//         a = (90 - alpha) * Math.PI / 180,
+//         x = xloc + R * Math.cos(a),
+//         y = yloc - R * Math.sin(a),
+//         path;
+//     if (total == value) {
+//         path = [
+//             ["M", xloc, yloc - R],
+//             ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+//         ];
+//     } else {
+//         path = [
+//             ["M", xloc, yloc - R],
+//             ["A", R, R, 0, +(alpha > 180), 1, x, y]
+//         ];
+//     }
+//     return {
+//         path: path
+//     };
+// };
+
+
+
+// archtype.circle(loc, loc, radius) // 59px radius makes room for 2px stroke
+//                 .attr({
+//                     stroke: '#e4e4e4',
+//                     'stroke-width': 5
+//                 });
+
+// archtype.path().attr({
+//    "stroke": "#f00",
+//    "stroke-width": 2,
+//    arc: [loc, loc, 0, 100, radius]
+//    }).animate({
+//        arc: [loc, loc, amount, 100, radius]
+//    }, 1500, "easeOut")
+//    ;
+
+//    var circlePing = archtype.circle(loc, loc, radius)
+//                 .attr({
+//                 	fill: "white",
+//                 	'fill-opacity': 0,
+//                     stroke: 'green',
+//                     'stroke-width': 2,
+//                     'stroke-opacity': 0
+//                 })
+//                 .hover(
+//                 function () {
+//                 	circlePing.animate({r: radius * 1.5, 
+// 			                 stroke: 'green',
+// 			                 'stroke-opacity': 1},
+// 			                1500,
+// 			                'bounce');  
+// 			    }, function () {
+// 			        circlePing.animate({r: radius, 
+// 			                 stroke: 'blue',
+// 			                 'stroke-opacity': 0},
+// 			                1500,
+// 			                'linear');
+// 			    });
---
+++ freelance/mt_old/js/skillCharts.js
@@ -0,0 +1,88 @@
+var o = {
+	init: function(elemId, name) {
+		this.diagram(elemId, name);
+	},
+
+	random: function(l, u) {
+		return Math.floor((Math.random()*(u - l + 1))+l);
+	},
+
+	diagram: function(elemId, name) {
+
+		// Figure out the size to display the charts at
+		// Each chart needs to scale with viewport size
+		var win_height = $(window).height();
+		var win_width = $(window).width();
+		// Set x and y base to be half of the containing div
+		var x_base = 250; // Original base was 250
+		var y_base = 170; // Original base was 170
+
+		var rad = 55;
+		var speed = 250; // Represents the speed of the animations in ms
+
+		if (win_width <= 360)
+			return; // We won't render the charts so there's no use in drawing them
+		else if (win_height <= 980) {
+			x_base = $('#web').width() / 2;
+			y_base = $('#web').height() / 2;
+			rad = 45;
+		}
+
+		var r = Raphael(elemId, 500, 400),
+			rad;
+
+		// The circle gets an x and y center point
+		r.circle(x_base, y_base, rad).attr({ stroke: 'none', fill: '#193340' });
+
+		var title;
+
+		r.customAttributes.arc = function(value, color, rad) {
+			var v = 3.6 * value,
+				alpha = v == 360 ? 359.99 : v,
+				random = 180, //o.random(91, 240),
+				a = (random - alpha) * Math.PI/180,
+				b = random * Math.PI/180,
+				sx = x_base + rad * Math.cos(b),
+				sy = y_base - rad * Math.sin(b),
+				x = x_base + rad * Math.cos(a),
+				y = y_base - rad * Math.sin(a),
+				path = [['M', sx, sy], ['A', rad, rad, 0, +(alpha > 180), 1, x, y]];
+			return { path: path, stroke: color }
+		}
+
+		$('#'+elemId+'Skills').find('.arc').each(function(i) {
+			var t = $(this),
+				color = t.find('.color').val(),
+				value = t.find('.percent').val(),
+				text = t.find('.text').text();
+			rad += 15;
+
+			var z = r.path().attr({arc: [value, color, rad], 'stroke-width': 20 });
+
+			z.mouseover(function() {
+				this.animate({'stroke-width': 25, opacity: .75 }, 1000, 'elastic');
+				title.animate({opacity: 0}, speed, '>', function() {
+					this.attr({text: text + '\n' + value + '%' }).animate({opacity: 1}, speed, '<');
+				});
+			}).mouseout(function() {
+				this.stop().animate({'stroke-width': 20, opacity: 1 }, 1000, 'elastic');
+				title.stop().animate({opacity: 0}, speed, '>', function() {
+					title.attr({ text: name}).animate({opacity: 1}, speed, '<');
+				});
+			});
+
+		});
+
+		title = r.text(x_base, y_base, name).attr({
+			font: '20px Arial',
+			fill: '#fff'
+		});
+	}
+}
+
+$(function() { 
+	o.init('web', 'Web');
+	o.init('prog', 'Programming');
+	o.init('devEnv', 'Dev Env.');
+	o.init('other', 'Other');
+});
